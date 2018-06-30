709c4491eb5decead8217ca9488dca4f4ffb9814
grandballoon <luke.sovern.conley@gmail.com>
2018-06-30 17:36:13 -0400

    bear lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 7, "m", "brown", "mean", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 4, "f", "black", "kind", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 10, "f", "blonde", "timid", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 9, "m", "black", "mean", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 8, "f", "black", "timid", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 14, "m", "brown", "kind", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 3, "f", "brown", "timid", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 5, "m", "black", "mean", 0);
