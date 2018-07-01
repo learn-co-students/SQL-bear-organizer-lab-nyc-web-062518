fb0e6d20fbb6caa8ca5f20eb7fc096c2f8b1f10c
cmaniscalchi <claudia.maniscalchi@gmail.com>
2018-07-01 14:30:47 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Bob", 10, "M", "black", "punny", 1),
+("Gene", 6, "M", "dark brown", "goofy", 1),
+("Tina", 19, "F", "black", "awkward", 0),
+("Linda", 13, "F", "dark brown", "ebullient", 1),
+("Louise", 2, "F", "black", "intense", 1),
+("Aunt Gayle", 16, "F", "brown", "neurotic", 1),
+("Teddy", 20, "M", "brown", "chatty", 0),
+(null, 20, "M", "black", "boring", 0);
